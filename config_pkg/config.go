26727b47a6f8db00832cb5280ee6382b6c415be9
hungnd <hungnd@navisoft.com.vn>
2026-07-04 00:32:55 +0700

    Feat: add config package

---
+++ config_pkg/config.go
@@ -0,0 +1,44 @@
+package configpkg
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int32  `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		Username string `yaml:"user"`
+		Password string `yaml:"pass"`
+	} `yaml:"database"`
+}
+
+var Host, Username, Password string
+var Port int32
+
+func Configpkg() {
+	f, err := os.Open("config.yaml")
+	if err != nil {
+		fmt.Println("Error while reading config.yaml")
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	var cfg Config
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		fmt.Println("Error while decoding config")
+		os.Exit(1)
+	}
+
+	Host = cfg.Server.Host
+	Port = cfg.Server.Port
+	Username = cfg.Database.Username
+	Password = cfg.Database.Password
+}
