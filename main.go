6ca64a64e67e4aac092f45297cc2b41a07854229
hungnd <hungnd@navisoft.com.vn>
2026-07-04 00:32:45 +0700

    Feat: add create new article api

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
 
+	configpkg "github.com/Hungnd562k/GoRestApiTutorial/config_pkg"
 	"github.com/gorilla/mux"
 )
 
@@ -77,15 +79,59 @@ func deleteArticleById(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Delete article with Id: ", key, "FAILED")
 }
 
+// Create new article
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: createNewArticle")
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error while reading request's payload", http.StatusBadGateway)
+		return
+	}
+	var jsonBody Article
+	err = json.Unmarshal(body, &jsonBody)
+	if err != nil {
+		http.Error(w, "Error while parsing request's payload", http.StatusBadRequest)
+	}
+	Articles = append(Articles, jsonBody)
+	fmt.Fprintf(w, "Create new article successfully!")
+}
+
+func updateArticleById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: updateArticleById")
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error while reading request's payload", http.StatusBadGateway)
+		return
+	}
+	var jsonBody Article
+	err = json.Unmarshal(body, &jsonBody)
+	if err != nil {
+		http.Error(w, "Error while parsing request's payload", http.StatusBadRequest)
+	}
+	id := jsonBody.Id
+	for index := range Articles {
+		if Articles[index].Id != id {
+			continue
+		}
+		Articles[index].Content = jsonBody.Content
+		Articles[index].Desc = jsonBody.Desc
+		Articles[index].Title = jsonBody.Title
+	}
+	fmt.Fprintf(w, "Update article successfully!")
+}
+
 // Handle incoming request
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/health", health)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/articles/{id}", returnArticleById)
-	myRouter.HandleFunc("/articles/delete/{id}", deleteArticleById)
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+	myRouter.HandleFunc("/health", health).Methods("GET")
+	myRouter.HandleFunc("/articles", returnAllArticles).Methods("GET")
+	myRouter.HandleFunc("/articles/{id}", returnArticleById).Methods("GET")
+	myRouter.HandleFunc("/articles/{id}", deleteArticleById).Methods("DELETE")
+	myRouter.HandleFunc("/articles", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles", updateArticleById).Methods("PUT")
+	address := fmt.Sprintf("%s:%s", string(configpkg.Host), strconv.Itoa(int(configpkg.Port)))
+	log.Fatal(http.ListenAndServe(address, myRouter))
 }
 
 func main() {
@@ -94,5 +140,10 @@ func main() {
 		{Id: 1, Title: "Đây là title 1", Desc: "Mô tả của title 1", Content: "Còn đây là nội dung của Title 1", Deleted: false},
 		{Id: 2, Title: "Đây là title 2", Desc: "Mô tả của title 2", Content: "Còn đây là nội dung của Title 2", Deleted: false},
 	}
+	configpkg.Configpkg()
+	fmt.Println("Host: ", configpkg.Host)
+	fmt.Println("Port: ", configpkg.Port)
+	fmt.Println("Username: ", configpkg.Username)
+	fmt.Println("Password: ", configpkg.Password)
 	handleRequests()
 }
